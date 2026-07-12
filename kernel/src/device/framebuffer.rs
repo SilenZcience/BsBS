1eafdaeaf4dddad3cb3fb20a96bc42baa0eaad17
Silas Kraume <silas.kraume1552@gmail.com>
2026-07-12 18:38:56 +0200

    vorlage lesson-6

--- kernel/src/device/framebuffer.rs
+++ kernel/src/device/framebuffer.rs
@@ -182,4 +182,27 @@ impl Framebuffer {
             buffer.add(move_bytes).write_bytes(0, total_bytes - move_bytes);
         }
     }
+
+    /// Draw a bitmap image at the specified (x, y) coordinates.
+    /// If the bitmap does not fully fit within the framebuffer, it is clipped.
+    pub fn draw_bitmap(&mut self, bitmap: &Bitmap, x: usize, y: usize) {
+        // Original bitmap dimensions
+        let bmp_width = bitmap.width() as usize;
+        let bmp_height = bitmap.height() as usize;
+
+        // Clip the bitmap to the framebuffer dimensions
+        let target_width = if x + bmp_width > self.width {
+            max(self.width - x, 0)
+        } else {
+            bmp_width
+        };
+
+        let target_height = if y + bmp_height > self.height {
+            max(self.height - y, 0)
+        } else {
+            bmp_height
+        };
+
+        todo!("framebuffer::draw_bitmap() is not yet implemented");
+    }
 }
