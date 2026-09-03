79ab41b3d5ac1f975df114693f3b20e0b51e5ea0
Silas Kraume <silas.kraume1552@gmail.com>
2026-09-03 15:55:27 +0200

    clear background on gameboy

--- kernel/src/shell/commands/gameboy.rs
+++ kernel/src/shell/commands/gameboy.rs
@@ -6,6 +6,7 @@ pub fn register() {
 }
 
 fn run(args: &[String]) {
+    crate::device::terminal::terminal().lock().clear();
     let rom = args.first().map(String::as_str).unwrap_or("roms/pokemon.gb");
     crate::demo::lesson6::peanut_gb::play(rom);
     crate::device::terminal::terminal().lock().clear();
